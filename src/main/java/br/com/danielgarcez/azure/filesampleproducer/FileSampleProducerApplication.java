0e958f79be01b5a1c3d47b6b713b74a0fcf8a5e1
danielgarcez78 <danielgarcez78@gmail.com>
2023-05-19 17:02:24 -0300

    Initial commit

---
+++ src/main/java/br/com/danielgarcez/azure/filesampleproducer/FileSampleProducerApplication.java
@@ -0,0 +1,13 @@
+package br.com.danielgarcez.azure.filesampleproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileSampleProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileSampleProducerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/danielgarcez/azure/filesampleproducer/FileSampleProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.danielgarcez.azure.filesampleproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileSampleProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
